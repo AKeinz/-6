dcb2a10db721e9e8751d1d09f6ab6be01aa801c8
AR <dostoevskijdinozavr@gmail.com>
2024-11-18 01:27:15 +0700

    реализация функций и изменение тестов

--- TestProject/ChangeMailTests.cs
+++ TestProject/ChangeMailTests.cs
@@ -6,36 +6,81 @@ namespace TestProject3
     public class ChangeMailTests
     {
         [TestMethod]
-        public void ReadFromFileTestMethod()
+        public void ReadFromNotExistingFileTestMethod()
         {
             ChangeMail mail = new ChangeMail();
-            string actual = mail.ReadFromFile("");
-            Assert.AreEqual(actual, "Файл  не найден!");
+            string actual = mail.ReadFromFile("notExistedFile");
+            string expected = "Файл notExistedFile не найден!";
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void CheckTestMethod()
+        public void CheckRightInputTestMethod()
         {
             ChangeMail mail = new ChangeMail();
-            string actual= mail.GetSeparatedValues("").text;
-            Assert.AreEqual(actual, "Impossible");
+            string input = "6,2\nHello\nas";
+            (int, int, string[]) actual= mail.GetSeparatedValues(input);
+            string[] expectedArray = input.Split('\n');
+            (int, int, string[]) expected = (6, 2, expectedArray);
+            Assert.AreEqual(expected.Item1, actual.Item1);
+            Assert.AreEqual(expected.Item2, actual.Item2);
+            Assert.AreEqual(string.Join(",", expected.Item3), string.Join(",", actual.Item3));
 
         }
 
         [TestMethod]
-        public void ChangeTextTestMethod()
+        public void CheckWrongKInputTestMethod()
         {
             ChangeMail mail = new ChangeMail();
-            string actual = mail.ChangeText((1, 1, ""));
-            Assert.AreEqual(actual, "Impossible");
+            string input = "2,2\nHello\nas";
+            (int, int, string[]) actual = mail.GetSeparatedValues(input);
+            string[] expectedArray = input.Split('\n');
+            (int, int, string[]) expected = (2, 2, expectedArray);
+            Assert.AreEqual(expected.Item1, actual.Item1);
+            Assert.AreEqual(expected.Item2, actual.Item2);
+            Assert.AreEqual("Impossible", string.Join(",", actual.Item3));
+
         }
 
         [TestMethod]
-        public void WriteToFileTestMethod()
+        public void CheckWrongNInputTestMethod()
         {
             ChangeMail mail = new ChangeMail();
-            bool actual = mail.WriteToFile("");
-            Assert.IsTrue(actual);
+            string input = "6,3\nHello\nas";
+            (int, int, string[]) actual = mail.GetSeparatedValues(input);
+            string[] expectedArray = input.Split('\n');
+            (int, int, string[]) expected = (6, 3, expectedArray);
+            Assert.AreEqual(expected.Item1, actual.Item1);
+            Assert.AreEqual(expected.Item2, actual.Item2);
+            Assert.AreEqual("Impossible", string.Join(",", actual.Item3));
+
         }
+
+        [TestMethod]
+        public void ChangeTextWithEvenKTestMethod()
+        {
+            ChangeMail mail = new ChangeMail();
+            string actual = mail.ChangeText((8, 3, ["8,3", "Hello", "World", "!"]));
+            string expected = "+Hello++\n+World++\n+++!++++";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ChangeTextWithOddKTestMethod()
+        {
+            ChangeMail mail = new ChangeMail();
+            string actual = mail.ChangeText((7, 3, ["7,3", "Hello", "World", "!"]));
+            string expected = "+Hello+\n+World+\n+++!+++";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void WriteToNotExistedFileTestMethod()
+        {
+            ChangeMail mail = new ChangeMail();
+            bool actual = mail.WriteToFile("notExistedFile", "");
+            Assert.IsFalse(actual);
+        }
+
     }
 }
--- Zadanie/ChangeMail.cs
+++ Zadanie/ChangeMail.cs
@@ -1,14 +1,81 @@
-﻿namespace Zadanie
+﻿using System.Security.Cryptography;
+
+namespace Zadanie
 {
     public class ChangeMail
     {
-        public string ReadFromFile(string path) { return $"Файл {path} не найден!"; }
+        public string ReadFromFile(string path) {
+            string result="";
+            if (!File.Exists(path)) { result = $"Файл {path} не найден!"; }
+            else
+            {
+                StreamReader file = new StreamReader(path);
+                result+= file.ReadToEnd();
+                file.Close();
+            }
+            return result ;
+        }
 
-        public (int K,int N,string text) GetSeparatedValues(string text) { return (0,0,"Impossible"); }
+        public (int K,int N,string[] text) GetSeparatedValues(string text)
+        {
+            string[] linesOfText = text.Split('\n');
+            string[] kAndN = linesOfText[0].Split(",");
+            int.TryParse(kAndN[0], out int k);
+            int.TryParse(kAndN[1], out int n);
+            if (CheckValues(k, n, linesOfText))
+            {
+                return (k, n, linesOfText);
+            }
+            return (k, n, ["Impossible"] ); 
+        }
 
-        public string ChangeText((int K, int N, string text) separValues) { return "Impossible"; }
+        public bool CheckValues(int k, int n, string[] linesOfText)
+        {
+            if (linesOfText.Length < 2) { return false; }
+            if ((k < 1 || k > 100) || (n < 1 || n > 1000)) { return false; }
+            if (n != linesOfText.Length - 1) { return false; }
+            foreach (string line in linesOfText.Skip(1))
+            {
+                if (line.Length > k) { return false; }
+            }
+            return true;
+        }
 
-        public bool WriteToFile(string path) { return false; }
+        public string ChangeText((int K, int N, string[] linesOfText) separValues) {
+            string separText = "";
+            if (!CheckValues(separValues.K, separValues.N, separValues.linesOfText)) { return "Impossible"; }
+            foreach (string line in separValues.linesOfText.Skip(1))
+            {
+                int diff = (separValues.K - line.Length) % 2;
+                string pluses = "";
+                for (int i = 1; i <= (separValues.K - line.Length) / 2; i++)
+                {
+                    pluses += "+";
+                }
+                switch (diff)
+                {
+                    case 0:
+                        separText += pluses + line + pluses;
+                        break;
+                    case 1:
+                        separText += pluses + line + pluses + "+";
+                        break;
+                }
+                if (!separValues.linesOfText.ElementAt(separValues.linesOfText.Length - 1).Equals(line)) { separText += "\n"; }
+            }
+            return separText; 
+        }
+
+        public bool WriteToFile(string path, string changedText) {
+            if (!File.Exists(path) || String.IsNullOrEmpty(changedText)) { return false; }
+            else
+            {
+                StreamWriter file = new StreamWriter(path);
+                file.WriteLine(changedText);
+                file.Close();
+                return true;
+            }
+        }
 
     }
 }
